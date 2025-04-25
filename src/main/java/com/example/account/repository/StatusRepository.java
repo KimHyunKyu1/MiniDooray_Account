29f73d6fcf7d69c24ed987ffe03d4db1f1ecf0cb
김현규 <khk15888@naver.com>
2025-04-25 15:23:26 +0900

    addStatusQuery

--- src/main/java/com/example/account/repository/StatusRepository.java
+++ src/main/java/com/example/account/repository/StatusRepository.java
@@ -3,6 +3,8 @@ package com.example.account.repository;
 import com.example.account.domain.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StatusRepository extends JpaRepository<Status, String> {
+import java.util.List;
 
+public interface StatusRepository extends JpaRepository<Status, String> {
+    List<Status> findAllByStatus(String status);
 }
--- src/main/java/com/example/account/service/StatusService.java
+++ src/main/java/com/example/account/service/StatusService.java
@@ -1,6 +1,22 @@
 package com.example.account.service;
 
-public class StatusService {
+import com.example.account.domain.Status;
+import com.example.account.repository.StatusRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class StatusService {
+    private StatusRepository statusRepository;
+    public Optional<Status> getStatus(String status) {
+        return statusRepository.findById(status);
+    }
+    public List<Status> findAll() {
+        return statusRepository.findAll();
+    }
 
 }
